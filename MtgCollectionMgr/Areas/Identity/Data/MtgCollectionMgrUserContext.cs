d4176fb7100b2742eb4e0f4a065ee87a33480275
Clarence Coppinger <thea.trio@gmail.com>
2019-02-08 13:24:17 -0600

    scaffolded identity

---
+++ MtgCollectionMgr/Areas/Identity/Data/MtgCollectionMgrUserContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MtgCollectionMgr.Areas.Identity.Data;
+
+namespace MtgCollectionMgr.Models
+{
+    public class MtgCollectionMgrUserContext : IdentityDbContext<MtgCollectionMgrUser>
+    {
+        public MtgCollectionMgrUserContext(DbContextOptions<MtgCollectionMgrUserContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
--- MtgCollectionMgr/Startup.cs
+++ MtgCollectionMgr/Startup.cs
@@ -55,6 +55,7 @@ namespace MtgCollectionMgr
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
